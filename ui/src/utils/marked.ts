4f4355c6d6250e1ca27e0c56fe8979d6ccbd585b
Toshiaki Maki <makingx@gmail.com>
2024-04-17 17:35:13 +0900

    Fix toc

--- ui/src/utils/marked.ts
+++ ui/src/utils/marked.ts
@@ -4,19 +4,22 @@ import {getHeadingList, gfmHeadingId, HeadingData} from "marked-gfm-heading-id";
 function toc(headings: HeadingData[]) {
     const contents: string[] = [];
     let previousLevel = -1;
-    contents.push('<ul id="table-of-content">')
+    contents.push('<ul id="table-of-content">');
+    let depth = 1;
     headings.forEach(({id, text, level}) => {
         if (previousLevel !== -1 && level > previousLevel) {
-            contents.push('<ul>')
+            contents.push('<ul>');
+            depth++;
         }
         if (level < previousLevel) {
-            contents.push('</ul>')
+            contents.push('</ul>');
+            depth--;
         }
         contents.push(`<li><a href="#${id}" class="h${level}">${text}</a></li>`);
         previousLevel = level;
     });
-    if (contents.length > 0) {
-        contents.push('</ul>')
+    while (depth-- > 0) {
+        contents.push('</ul>');
     }
     return contents.join('');
 }
