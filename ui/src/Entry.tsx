115fd0e634aced71729cf001ac3083495d98e246
Toshiaki Maki <makingx@gmail.com>
2024-04-22 23:36:18 +0900

    Update Category

--- ui/src/Entry.tsx
+++ ui/src/Entry.tsx
@@ -8,13 +8,14 @@ import {addCopyButton} from './utils/copy';
 import marked from './utils/marked.ts'
 import 'highlight.js/styles/default.min.css';
 import {styled} from "styled-components";
+import Category from "./components/Category.tsx";
 
 export interface EntryProps {
     preLoadedEntry: EntryModel;
 }
 
 const Title = styled.h3`
-  font-size: 1.25rem;
+  font-size: 1.5rem;
   margin: 0 0 0.5rem;
 
   a {
@@ -53,15 +54,12 @@ const Entry: React.FC<EntryProps> = ({preLoadedEntry}) => {
         return <Loading/>
     }
     const contentHtml = marked.parse(entry.content, {async: false, gfm: true}) as string;
-    const categories = entry.frontMatter.categories
-        .map<React.ReactNode>(c => <Link key={c.name} to={`/categories`}>{c.name}</Link>)
-        .reduce((prev, curr) => [prev, ' > ', curr]);
     const tags = entry.frontMatter.tags
         .map<React.ReactNode>(t => <Link key={t.name}
                                          to={`/tags/${t.name}/entries`}>{t.name}</Link>)
         .reduce((prev, curr) => [prev, ' | ', curr]);
     return <>
-        <p id="entry-categories">{categories}</p>
+        <p id="entry-categories"><Category categories={entry.frontMatter.categories}/></p>
         <Title id="entry-title"><Link to={`/entries/${entry.entryId}`}>{entry.frontMatter.title}</Link></Title>
         <Meta id="entry-meta">
             Created on <span title={entry.created.date}>{new Date(entry.created.date).toDateString()}</span> •
---
+++ ui/src/components/Category.tsx
@@ -0,0 +1,27 @@
+import {Link} from "react-router-dom";
+import {Category as CategoryModel} from "../types.ts";
+import React from "react";
+
+interface CategoryProps {
+    categories: CategoryModel[]
+}
+
+const Category: React.FC<CategoryProps> = ({categories}) => {
+    const categoriesList: string[] = [];
+    const links: React.ReactNode[] = [];
+    categories.forEach(c => {
+        categoriesList.push(c.name);
+        const segment = categoriesList.join(',');
+        links.push(link(segment, categoriesList));
+        links.push(<span key={segment + '-slash'}>{` > `}</span>);
+    });
+    links.pop();
+    return links;
+}
+
+function link(segment: string, categories: string[]): React.ReactNode {
+    const name = categories[categories.length - 1];
+    return <Link key={segment} to={`/categories/${segment}/entries`}>{`${name}`}</Link>;
+}
+
+export default Category;
