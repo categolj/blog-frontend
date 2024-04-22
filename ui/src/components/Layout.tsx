cb23773a0e90c8e70643e467f0e1a9430f7724c9
Toshiaki Maki <makingx@gmail.com>
2024-04-23 01:25:02 +0900

    Add Navi

--- ui/src/components/Layout.tsx
+++ ui/src/components/Layout.tsx
@@ -1,10 +1,13 @@
 import Header from './Header.tsx';
 import {Outlet} from 'react-router-dom';
 import Footer from "./Footer.tsx";
+import Navi from "./Navi.tsx";
 
 const Layout = () => {
     return <>
         <Header/>
+        <Navi/>
+        <hr/>
         <main>
             <Outlet/>
         </main>
---
+++ ui/src/components/Navi.tsx
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom';
+import {styled} from "styled-components";
+
+const NaviList = styled.ul`
+  margin: 0;
+
+  li {
+    list-style: none;
+    display: inline;
+    padding-right: 1em;
+  }
+`;
+
+const Navi = () => {
+    return <>
+        <NaviList id='navi'>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/tags'}>Tags</Link></li>
+        </NaviList>
+    </>;
+};
+
+export default Navi;
