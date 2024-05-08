c767187128c1ccca9f59ceb5cd0c7b195ef37d34
Toshiaki Maki <makingx@gmail.com>
2024-05-08 16:40:05 +0900

    styling message box

--- ui/src/components/Message.tsx
+++ ui/src/components/Message.tsx
@@ -1,4 +1,29 @@
 import React, {ReactElement} from "react";
+import {styled} from "styled-components";
+
+const colors = {
+    success: '#5CBD9D',
+    error: '#E74C3C',
+    warning: '#F29C33',
+    info: '#3998DB',
+};
+
+const MessageBox = styled.p<MessageProps>`
+  padding: 1rem;
+  margin-bottom: 1rem;
+  border: 1px solid ${(props) => colors[props.status]};
+  border-radius: 0.25rem;
+  color: #333;
+  gap: 0.5rem;
+  line-height: 1.5;
+  width: 800px;
+  max-width: 100%;
+  border-left: 5px solid ${(props) => colors[props.status]};
+
+  strong {
+    color: ${(props) => colors[props.status]};
+  }
+`;
 
 
 export interface MessageProps {
@@ -7,7 +32,7 @@ export interface MessageProps {
 }
 
 const Message: React.FC<MessageProps> = ({status, text}) => {
-    return text && <p><strong>{status}</strong>: {text}</p>;
+    return text && <MessageBox status={status} text={null}><strong>{status}</strong> {text}</MessageBox>;
 
 };
 
