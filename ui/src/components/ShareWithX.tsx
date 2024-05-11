5df493ca24e4d9941b486b27e9eb3439d0f918ff
Toshiaki Maki <makingx@gmail.com>
2024-05-11 20:14:04 +0900

    Fix

--- ui/src/components/ShareWithX.tsx
+++ ui/src/components/ShareWithX.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import {X} from "../styled/X.tsx";
-
+import {styled} from "styled-components";
 interface ShareWithXProps {
     url: string,
     text: string,
 }
+
+const X = styled.a`
+  background-color: black;
+  background-image: url(/X.svg);
+  background-repeat: no-repeat;
+  background-size: contain;
+  border-radius: 4px;
+  display: block;
+  height: 32px;
+  transition: background-color .2s;
+  width: 32px;
+`
 
 export const ShareWithX: React.FC<ShareWithXProps> = ({
                                                           url,
