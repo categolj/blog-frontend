026614b6e7c6c14033c9cd0a29740be2350fc3f5
Toshiaki Maki <makingx@gmail.com>
2024-08-01 15:48:36 +0900

    If the IP is blacklisted, the counter will not increment.

--- src/main/java/am/ik/blog/CounterApiProps.java
+++ src/main/java/am/ik/blog/CounterApiProps.java
@@ -1,7 +1,9 @@
 package am.ik.blog;
 
+import java.util.List;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "counter-api")
-public record CounterApiProps(String url) {
+public record CounterApiProps(String url, List<String> ipBlackList) {
 }
--- src/main/java/am/ik/blog/counter/CounterController.java
+++ src/main/java/am/ik/blog/counter/CounterController.java
@@ -2,19 +2,30 @@ package am.ik.blog.counter;
 
 import java.util.Locale;
 
+import am.ik.blog.CounterApiProps;
+import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
 public class CounterController {
 
 	private final CounterClient counterClient;
 
-	public CounterController(CounterClient counterClient) {
+	private final CounterApiProps props;
+
+	public CounterController(CounterClient counterClient, CounterApiProps props) {
 		this.counterClient = counterClient;
+		this.props = props;
 	}
 
 	@PostMapping(path = "/api/counter")
@@ -23,7 +34,27 @@ public class CounterController {
 		if (userAgent.toLowerCase(Locale.ROOT).contains("bot")) {
 			return new Counter(0);
 		}
+		String ipAddress = getIpAddress();
+		if (!CollectionUtils.isEmpty(props.ipBlackList()) && ipAddress != null) {
+			if (props.ipBlackList().contains(ipAddress)) {
+				return new Counter(0);
+			}
+		}
 		return this.counterClient.increment(request);
+	}
+
+	@Nullable
+	static String getIpAddress() {
+		RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+		if (!(requestAttributes instanceof ServletRequestAttributes)) {
+			return null;
+		}
+		HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+		String xForwardedFor = request.getHeader("X-Forwarded-For");
+		if (xForwardedFor != null) {
+			return xForwardedFor;
+		}
+		return request.getRemoteAddr();
 	}
 
 }
